b973d0f3393364e84ee853e192b46e4625e62999
fmaciasceb <ferninfa@hotmail.com>
2016-07-31 22:00:45 +0200

    Add files via upload

---
+++ [MaciasCeballos.Fernando].[MyRecipes]/MyWebRecipe/Default.aspx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using RecipeBook;
+using System.Data;
+
+namespace MyWebRecipe
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //RecipeBook.RecipeBook cp = new RecipeBook.RecipeBook(); //Create new recipebook
+            //DataTable dt = cp.GiveMeDataTable("https://channel9.msdn.com/feeds/rss");
+            //this.GridView1.DataSource = dt;
+            //this.GridView1.DataBind();
+            //if (this.GridView1.Columns.Count > 0)
+            //{
+            //    this.GridView1.Columns[0].Visible = false;
+            //}
+        }
+    }
+}
---
+++ [MaciasCeballos.Fernando].[MyRecipes]/MyWebRecipe/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MyWebRecipe.Startup))]
+namespace MyWebRecipe
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
